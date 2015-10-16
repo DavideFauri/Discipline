9a8cb34f8873834065e9166c105507c769206a99
Davide Fauri <davide.fauri@gmail.com>
2015-10-16 19:22:37 +0200

    Complete overhaul: - simplified every step of installation - now the default is to install in the user Library - added section on how to block LaunchControl because apparently it's too easy to disable the agents and wait them out

---
+++ 3 - copy script to Script Libraries/launch_me_to_copy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p ~/Library/Script\ Libraries
+
+BASEDIR="$( dirname "$0" )"
+cd "$BASEDIR"
+
+cp Discipline.scpt ~/Library/Script\ Libraries
---
+++ 4 - copy templates to LaunchAgents/launch_me_to_copy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p ~/Library/LaunchAgents
+
+BASEDIR="$( dirname "$0" )"
+cd "$BASEDIR"
+
+cp not\ this\ folder,\ just\ the\ contents/.  ~/Library/LaunchAgents/

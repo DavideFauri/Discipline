5ef947ed49ef8153c0671427db53580f8e5dc208
Davide Fauri <davide.fauri@gmail.com>
2020-03-02 06:32:31 +0100

    Add grace time, error reporting via notification, start_block

--- discipline.py
+++ discipline.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
+from time import time, sleep
 import argparse
 import sys
 from os import getenv
 import subprocess
 import re
+
+GRACE_TIME = 5
 
 
 def parse_args():
@@ -33,7 +36,7 @@ def parse_args():
     return arg_parser.parse_args()
 
 
-def notify(message, title):
+def notify(message="", title="Discipline"):
     subprocess.run(
         ["osascript", "-e", f'display notification "{message}" with title "{title}"']
     )
@@ -53,6 +56,9 @@ def is_block_active(whoami):
         capture_output=True,
         text=True,
     )
+    if start_time.returncode != 0:
+        notify(message="Could not get block starting date!", title="Error!")
+        exit(1)
     return start_time.stdout != "4001-01-01 00:00:00 +0000\n"
 
 
@@ -73,8 +79,50 @@ def is_current_SSID_within(list_SSIDs):
             return True
     return False
 
+
+def compute_block_duration(block_end):
+    return 2
+
+
+def set_block_duration(whoami, minutes):
+    result = subprocess.run(
+        [
+            "sudo",
+            "-u",
+            whoami,
+            "defaults",
+            "write",
+            "org.eyebeam.SelfControl",
+            "BlockDuration",
+            "-int",
+            str(minutes),
+        ]
+    )
+    if result.returncode != 0:
+        notify(message="Could not set block duration!", title="Error!")
+        exit(1)
+
+
+def start_block(whoami):
+    my_id = subprocess.run(["id", "-u", whoami], capture_output=True, text=True).stdout[
+        :-1
+    ]
+    result = subprocess.run(
+        [
+            "sudo",
+            "/Applications/SelfControl.app/Contents/MacOS/org.eyebeam.SelfControl",
+            my_id,
+            "--install",
+        ]
+    )
+    if result.returncode != 0:
+        notify(message="Could not start block!", title="Error!")
+        exit(1)
+
+
 if __name__ == "__main__":
     args = parse_args()
+    block_end = args.time[0]
 
     # check for root
     whoami = getenv("USER")
@@ -90,6 +138,21 @@ if __name__ == "__main__":
     # check SSID condition
     if args.SSID is not None and not is_current_SSID_within(args.SSID):
         exit(0)  # quit silently
+
+    # get duration and check on duration
+    minutes = compute_block_duration(block_end)
+    if minutes <= 0 or minutes > 12 * 60:
+        exit(0)  # quit silently
+
+    # start block
+    notify(
+        message=f"Start working in {GRACE_TIME} seconds!",
+        title=f"Discipline until {block_end}",
+    )
+    sleep(GRACE_TIME)
+
+    set_block_duration(whoami, minutes)
+    start_block(whoami)
 
 
 # # write down starting time

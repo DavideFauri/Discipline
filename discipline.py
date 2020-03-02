c9745ab4b21c21bbf31a3fed2ffb25c9984d8c72
Davide Fauri <davide.fauri@gmail.com>
2020-03-02 05:08:31 +0100

    Add parsing of arguments

--- discipline.py
+++ discipline.py
@@ -1,1 +1,36 @@
 #!/usr/bin/env python3
+
+import argparse
+import sys
+
+def parse_args():
+    arg_parser = argparse.ArgumentParser()
+
+    arg_parser.add_argument(
+        "-u",
+        "--until",
+        dest="time",
+        action="store",
+        help="Block end time [hh:mm]",
+        nargs=1,
+    )
+    arg_parser.add_argument(
+        "-n",
+        "--network",
+        dest="SSID",
+        action="append",
+        help="List of applicable WiFi SSIDs",
+    )
+
+    if len(sys.argv) == 1:
+        arg_parser.print_help(sys.stderr)
+        sys.exit(1)
+
+    return arg_parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+
+
+# # write down end time

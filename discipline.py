0e5f5000082bbfdd25c7e36f68cee825256fdeaa
Davide Fauri <davide.fauri@gmail.com>
2020-03-02 05:43:43 +0100

    Check that I am on disciplined WiFi

--- discipline.py
+++ discipline.py
@@ -3,6 +3,7 @@
 import argparse
 import sys
 import subprocess
+import re
 
 
 def parse_args():
@@ -45,7 +46,22 @@ def is_block_active():
     return start_time.stdout != "4001-01-01 00:00:00 +0000\n"
 
 
+def is_current_SSID_within(list_SSIDs):
+    airport_out = subprocess.run(
+        [
+            "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport",
+            "-I",
+        ],
+        capture_output=True,
+        text=True,
     )
+
+    current_SSID = re.findall(" SSID: (.*)", airport_out.stdout)[0]
+
+    for SSID in list_SSIDs:
+        if current_SSID == SSID:
+            return True
+    return False
 
 if __name__ == "__main__":
     args = parse_args()
@@ -53,6 +69,11 @@ if __name__ == "__main__":
     if is_block_active():
         notify(message="Block already running!", title="Discipline active")
         exit(0)
+
+    # check SSID condition
+    if args.SSID is not None and not is_current_SSID_within(args.SSID):
+        exit(0)  # quit silently
+
 
 # # write down starting time
 
